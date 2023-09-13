d99308c83ad3c2da2141f23c9a1229dec30a66dc
aman <amankumar.sharma199@gmail.com>
2023-09-13 20:29:54 +0530

    user model created

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
